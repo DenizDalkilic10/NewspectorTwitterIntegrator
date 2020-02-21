cf6e44143dbf8e3547186ee34207eb53ee3fff83
Deniz Dalkılıç <denizdalkilic1998@gmail.com>
2020-02-21 16:05:41 +0300

    Loading and Saving UserId-Tweet Id map done

--- Main.py
+++ Main.py
@@ -1,12 +1,16 @@
 # This class will call service manager object and perform operations in every xxx minute until shutdown
 from Services import TwitterServices
 
-twitter_service = TwitterServices.TwitterServices("C:/Users/user/PycharmProjects/NewspectorNewsFetcher/Resources"
-                                                  "/NewsSources.json")
-for i in twitter_service.map:
+accounts_resource = "Resources/NewsSources.json"
+user_tweet_map_resource = "Resources/UserTweetMap.json"
+
+twitter_service = TwitterServices.TwitterServices(accounts_resource, user_tweet_map_resource)
+
+for i in twitter_service.user_tweet_map:
     profile = twitter_service.fetch_account_info(i)
-    tweets = twitter_service.fetch_latest_tweets_from_account(i, 1, twitter_service.map[i])
-    # for tweet in tweets:
-    #     print(i + " " + tweet["tweetId"])
-    twitter_service.update_map(i, int(tweets[len(tweets)-1]["tweetId"]))
-    #print(twitter_service.map)
+    tweets = twitter_service.fetch_latest_tweets_from_account(i, 1, twitter_service.user_tweet_map[i])
+    if len(tweets) != 0:
+        twitter_service.update_map(i, int(tweets[0]["tweetId"]))
+    for tweet in tweets:
+        print("Account: " + i + " | Tweet: " + tweet["text"] + " | " + tweet["tweetId"])
+twitter_service.save_map_to_resources(twitter_service.user_tweet_map, user_tweet_map_resource)
--- Services/TwitterServices.py
+++ Services/TwitterServices.py
@@ -1,30 +1,48 @@
 # This class will handle tweet fetching by using the twitter api
 import json
+import os
 from twitter_scraper import get_tweets
 from twitter_scraper import Profile
 
 
 class TwitterServices:
 
-    def __init__(self, account_names_resource):
-        self.account_names_resource = account_names_resource  # sets the path for the resource
-        self.map = self.init_map()  # inits the map of (account , last fetched tweet id)
+    def __init__(self, accounts_resource, user_tweet_map_resource):
+        self.accounts_resource = accounts_resource  # sets the path for the resource
+        self.user_tweet_map_resource = user_tweet_map_resource
+        self.user_tweet_map = self.init_map()  # inits the map of (account , last fetched tweet id)
 
     def get_account_names_from_source(self):
-        with open(self.account_names_resource) as json_file:
+        with open(self.accounts_resource) as json_file:
             data = json.load(json_file)
             account_list = data["accountNames"]
         return account_list
 
     def init_map(self):
-        account_names = self.get_account_names_from_source()
-        map_of_account_name_and_id = {}
-        for name in account_names:
-            map_of_account_name_and_id.update({name: 0})
-        return map_of_account_name_and_id
+        user_tweet_map = self.load_map_from_resources()
+        if user_tweet_map is not None:
+            return user_tweet_map
+        else:
+            account_names = self.get_account_names_from_source()
+            user_tweet_map = {}
+            for name in account_names:
+                user_tweet_map.update({name: 0})
+            return user_tweet_map
+
+    def save_map_to_resources(self, user_tweet_map, resource):
+        with open(resource, 'w') as outfile:
+            json.dump(user_tweet_map, outfile)
+
+
+    def load_map_from_resources(self):
+        if os.stat(self.user_tweet_map_resource).st_size == 0:
+            return None
+        else:
+            with open(self.user_tweet_map_resource) as json_file:
+                return json.load(json_file)
 
     def update_map(self, account_name, last_fetched_tweet_id):  # updates the map after fetch operation on source  done
-        return self.map.update({account_name: last_fetched_tweet_id})
+        return self.user_tweet_map.update({account_name: last_fetched_tweet_id})
 
     def filter_tweets_since_id(self, tweets, tweet_id):
         def filter_tweets(tweet):
@@ -41,4 +59,3 @@ class TwitterServices:
 
     def fetch_account_info(self, account_name):
         return Profile(account_name)
-
